cde11d19ecb234d1183e32100044ad382e198266
Allan Taylor <allan@startmail.com>
2021-10-01 10:48:18 -0400

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -3,6 +3,6 @@ terraform {
     resource_group_name  = "pma"
     storage_account_name = "allantaylorpma"
     container_name       = "terraform"
-    key                  = "terraform.tfstate"
+    key                  = "spoke-workspace.tfstate"
   }
 }
