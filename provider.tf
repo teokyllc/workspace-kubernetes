6e135b1ec79276d4edc8ce496c7b90dc7dcad685
Allan Taylor <allan@startmail.com>
2021-09-30 23:41:00 -0400

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.78.0"
+    }
+  }
+}
+
 provider "azurerm" {
   subscription_id = var.subscription_id
   tenant_id       = var.aad_tenant_id
